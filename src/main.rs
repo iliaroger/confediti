b7b4d4eb6ea2ff3a7280cdbe7a3e9576bb4f444f
Ilia Roger <iliailia@me.com>
2024-01-31 22:13:46 +0300

    fix: added draw board and minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
-use clap::{Arg, ArgMatches, Command, Subcommand};
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Subcommand)]
+enum Commands {
+    Test { list: bool },
+    Check { name: String },
+}
+#[derive(Parser)]
+#[command(
+    author = "ilya roger. <iliailia@me.com>", 
+    version = "0.1.0", 
+    about = "Simplify migrating your config files from nvim, tmxu and more onto new machines", 
+    long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "File")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
 fn main() {
-    let matches = Command::new("confucius")
-        .version("0.1")
-        .author("Ilya Roger")
-        .about("Create, update and migrate your configs hasle free.")
-        .subcommand(Command::new("get"))
-        .about("gets the config files and installs them into your machine")
-        .arg(Arg::new("url"))
-        .short_flag('u')
-        .long_flag("url")
-        .subcommand_value_name("URL");
+    let cli = Cli::parse();
+
+    if let Some(name) = cli.name.as_deref() {
+        println!("value for name: {name}")
+    }
+
+    match cli.debug {
+        1 => println!("occured once"),
+        _ => println!("occured too many times"),
+    }
 }
